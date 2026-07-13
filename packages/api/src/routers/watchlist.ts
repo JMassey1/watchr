7c48aa24482ab9b50d8617b361efcb7007c0727b
Jordan Massey <j.massey2278@gmail.com>
2026-07-13 00:23:07 -0500

    added route for getting lists, added TODO

--- packages/api/src/routers/watchlist.ts
+++ packages/api/src/routers/watchlist.ts
@@ -1,7 +1,15 @@
-import { count, eq } from "drizzle-orm";
+import {and, count, eq, sql} from "drizzle-orm";
 import {protectedProcedure, router} from "../index";
 import {db} from "@watch3r/db";
-import {watchlistMember} from "@watch3r/db/schema/watchlist";
+import {watchlist, watchlistInsertSchema, watchlistMember} from "@watch3r/db/schema/watchlist";
+import {z} from "zod";
+
+/*
+TODO:
+- Delete list (needs to check role of user, admin+ to do it)
+- Add member (^^)
+- Remove member (^^)
+*/
 
 export const watchlistRouter = router({
 	myWatchlistCount: protectedProcedure.query(async ({ ctx }) => {
@@ -11,5 +19,37 @@ export const watchlistRouter = router({
 			.where(eq(watchlistMember.userId, ctx.session.user.id));
 
 		return { count: row?.value ?? 0 };
-	})
+	}),
+	myWatchlists: protectedProcedure
+		.input(z
+			.object({
+				ownedOnly: z.boolean().optional().default(false)
+			}).optional(),
+		)
+		.query(async ({ ctx, input }) => {
+			const userId = ctx.session.user.id;
+
+			return db
+				.select({
+					id: watchlist.id,
+					name: watchlist.name,
+					owner: watchlist.ownerId,
+					isOwner: sql<boolean>`${watchlist.ownerId} = ${userId}`
+				})
+				.from(watchlistMember)
+				.innerJoin(watchlist, eq(watchlistMember.watchlistId, watchlist.id))
+				.where(
+					input?.ownedOnly
+						? and(
+							eq(watchlistMember.userId, userId),
+							eq(watchlist.ownerId, userId),
+							)
+						: eq(watchlistMember.userId, userId)
+				);
+		}),
+	create: protectedProcedure
+		.input(watchlistInsertSchema.omit({ ownerId: true}))
+		.mutation(async ({ ctx, input }) => {
+			return db.insert(watchlist).values({...input, ownerId: ctx.session.user.id}).returning();
+		})
 })
