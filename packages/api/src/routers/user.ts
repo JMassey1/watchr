2b909f7bf06adcbab9a52b64ad4fb237e23e94be
Jordan Massey <j.massey2278@gmail.com>
2026-07-13 19:16:16 -0500

    added userRouter with searchUsers function

---
+++ packages/api/src/routers/user.ts
@@ -0,0 +1,44 @@
+import {protectedProcedure, router} from "../index";
+import {z} from "zod";
+import {db} from "@watch3r/db";
+import {watchlistMember} from "@watch3r/db/schema/watchlist";
+import {user} from "@watch3r/db/schema/auth";
+import {and, eq, ilike, or, notInArray} from "drizzle-orm";
+
+export const userRouter = router({
+	searchUsers: protectedProcedure
+		.input(
+			z.object({
+				watchlistId: z.number().optional(),
+				query: z.string().trim().min(2).max(100),
+				limit: z.number().min(1).max(20).default(10)
+			}),
+		)
+		.query(async ({ctx, input}) => {
+			const excludeIds = [ctx.session.user.id];
+			if (input.watchlistId) {
+				const existing = await db
+					.select({userId: watchlistMember.userId})
+					.from(watchlistMember)
+					.where(eq(watchlistMember.watchlistId, input.watchlistId));
+				existing.map((member) => {
+					excludeIds.push(member.userId);
+				})
+			}
+
+			const term = `%${input.query}%`;
+			return db
+				.select({userId: user.id, name: user.name, image: user.image})
+				.from(user)
+				.where(
+					and(
+						or(ilike(user.name, term), ilike(user.email, term)),
+						excludeIds.length > 0
+							? notInArray(user.id, excludeIds)
+							: undefined,
+					),
+				)
+				.limit(input.limit);
+
+		})
+})
